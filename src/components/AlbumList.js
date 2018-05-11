7b0a3734a8273d1a5cc5a251fef70846841f975e
Nathan Marley <nathan.marley@gmail.com>
2018-05-11 16:15:10 -0700

    add AlbumList component

---
+++ src/components/AlbumList.js
@@ -0,0 +1,15 @@
+// Import libraries
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// Create Component
+const AlbumList = () => {
+  return (
+    <View>
+      <Text>Album List!!!!!</Text>
+    </View>
+  );
+};
+
+// export Component for re-use
+export default AlbumList;
