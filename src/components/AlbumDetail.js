00aea856e2d696820919f2939709c92273952173
Nathan Marley <nathan.marley@gmail.com>
2018-05-12 09:21:46 -0700

    add a button w/nice styling

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { View, Text, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 // Create
 const AlbumDetail = ({ album }) => {
@@ -29,11 +30,16 @@ const AlbumDetail = ({ album }) => {
           <Text>{artist}</Text>
         </View>
       </CardSection>
+
       <CardSection>
         <Image
           style={imageStyle}
           source={{ uri: image }}
         />
+      </CardSection>
+
+      <CardSection>
+        <Button />
       </CardSection>
     </Card>
   );
