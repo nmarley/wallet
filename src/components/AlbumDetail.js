f2a7da9d87f49361e98a0b66a340c517d0d46a72
Nathan Marley <nathan.marley@gmail.com>
2018-05-11 22:06:59 -0700

    add a card component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,13 +1,14 @@
 // Import
 import React from 'react';
 import { View, Text } from 'react-native';
+import Card from './Card'
 
 // Create
 const AlbumDetail = (props) => {
   return (
-    <View>
+    <Card>
       <Text>{props.album.title}</Text>
-    </View>
+    </Card>
   );
 };
 
