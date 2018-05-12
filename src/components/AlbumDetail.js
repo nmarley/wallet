685fe800998a2ef9eedc3f3de12608c7c085330a
Nathan Marley <nathan.marley@gmail.com>
2018-05-11 22:29:05 -0700

    display images and style card sections

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,19 +1,42 @@
 // Import
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import Card from './Card'
 import CardSection from './CardSection'
 
 // Create
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image } = album;
+
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View>
+          <Image
+            style={styles.thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={styles.headerContentStyle}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
       </CardSection>
     </Card>
   );
 };
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  }
+}
+
 
 // export
 export default AlbumDetail;
