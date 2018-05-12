1ee9926452e860ae13dd29fafbc5dfa37e53e727
Nathan Marley <nathan.marley@gmail.com>
2018-05-12 07:28:40 -0700

    enable scrolling

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,6 @@
 // Import libraries
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
@@ -23,9 +23,9 @@ class AlbumList extends Component {
     console.log(this.state);
 
     return (
-      <View>
+      <ScrollView>
         {this.renderAlbums()}
-      </View>
+      </ScrollView>
     );
   }
 }
